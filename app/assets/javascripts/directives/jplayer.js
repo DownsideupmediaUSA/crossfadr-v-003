f05975764dd8fdbe9e252e11aa4a42bcf8d38e0f
DownsideupmediaUSA <brandondecarlo@live.com>
2017-04-14 07:56:16 -0400

    minor changes to buttons

--- app/assets/javascripts/directives/jplayer.js
+++ app/assets/javascripts/directives/jplayer.js
@@ -3,72 +3,57 @@
   'use strict';
 
 
-   app.module(crossfadr)
-      .directive("jplayer", ['$window', 'PlayerService', function ($window, PlayerService) {
-     return {
-     restrict: "E",
-     // Have our own scope - we only want to watch the service and not conflict with other scopes
-     scope: {},
-     // Serve up some html with our player
-     templateUrl: "/jplayer-template.html",
-     link: function (scope, element, attrs) {
-         // An element on the page to attach the jPlayer to. Could also use "element" from linkFN ^
-         var jPlayer = angular.element("#jquery_jplayer_1").jPlayer();
+  app.module(crossfadr)
+     .directive('jplayer', function() {
+       return {
+       restrict: 'EA',
+       template: '<div></div>',
+       link: function(scope, element, attrs) {
+         var $control = element,
+             $player = $control.children('div'),
+             cls = 'pause';
 
-         // Point to the css
-         $window.jplayerCss = "#jp_container_1";
-         // Set up a playlist
-         $window.myPlaylist = new jPlayerPlaylist({
-             jPlayer: "#jquery_jplayer_1",
-             cssSelectorAncestor: jplayerCss,
-             repeat: function (e) {
-                 // Implement repeat from the service
+         var updatePlayer = function() {
+           $player.jPlayer({
+             // Flash fallback for outdated browser not supporting HTML5 audio/video tags
+             // http://jplayer.org/download/
+             swfPath: 'js/jplayer/',
+             supplied: 'mp3',
+             solution: 'html, flash',
+             preload: 'auto',
+             wmode: 'window',
+             ready: function () {
+               $player
+                 .jPlayer("setMedia", {mp3: scope.$eval(attrs.audio)})
+                 .jPlayer(attrs.autoplay === 'true' ? 'play' : 'stop');
+             },
+             play: function() {
+               $control.addClass(cls);
+
+               if (attrs.pauseothers === 'true') {
+                 $player.jPlayer('pauseOthers');
+               }
+             },
+             pause: function() {
+               $control.removeClass(cls);
+             },
+             stop: function() {
+               $control.removeClass(cls);
+             },
+             ended: function() {
+               $control.removeClass(cls);
              }
-         }, [], { supplied: 'mp3', swfPath: "obj/", free: true }, 0);
+           })
+           .end()
+           .unbind('click').click(function(e) {
+             $player.jPlayer($control.hasClass(cls) ? 'stop' : 'play');
+           });
+         };
 
-         // Add the player service to the scope so we can watch stuff!
-         scope.PlayerService = PlayerService;
-
-         // When the Current track (on the service) changes - we want to tell jPlayer to play that new song
-         scope.$watch('PlayerService.CurrentTrack', function (value) {
-             if (value != null) {
-                 jPlayer.jPlayer('setMedia', {
-                     // The url of the mp3 file
-                     mp3: value.StreamUri,
-                     title: value.Title
-                 }).jPlayer('play');
-             }
-         });
-
-         // Pausing is controlled from a service (so many things can toggle it).
-         // So watch it an control jPlayer when it changes
-         scope.$watch('PlayerService.IsPaused', function (value) {
-             if (value != null) {
-                 if (value == true) {
-                     jPlayer.jPlayer('pause');
-                 }
-                 else {
-                     jPlayer.jPlayer('play');
-                 }
-             }
-         });
-
-         jPlayer.bind($.jPlayer.event.ended, function (event) {
-             // Song has ended, try to go next
-             if (scope.PlayerService.HasNext) {
-                 scope.PlayerService.Next();
-             }
-         });
-
-         scope.$on('$destroy', function () {
-             // Clean up memory from the events
-             jPlayer.unbind($.jPlayer.event.ended);
-             jPlayer.unbind($.jplayer.event.play);
-             // Don't think we'll ever destroy it (it's on every page) - and why would you want to?
-             jPlayer.jPlayer('destroy');
-         });
+         scope.$watch(attrs.audio, updatePlayer);
+         updatePlayer();
        }
      };
-  }]);
+  });
 
 })
--- app/assets/javascripts/directives/jplayerService.js
+++ app/assets/javascripts/directives/jplayerService.js
@@ -1,20 +1,20 @@
-(function() {
-
-  'use strict';
-
-
-   app.module(crossfadr)
-      .service("jplayerService", [function () {
-    this.IsPaused = false;
-    this.CurrentTrack = null;
-
-    this.Play = function (track) {
-    this.CurrentTrack = track;
-    this.IsPaused = false;
-   };
-    this.Pause = function () {
-       this.IsPaused = !this.IsPaused;
-   };
-  }]);
-
-})
+// (function() {
+//
+//   'use strict';
+//
+//
+//    app.module(crossfadr)
+//       .service("jplayerService", [function () {
+//     this.IsPaused = false;
+//     this.CurrentTrack = null;
+//
+//     this.Play = function (track) {
+//     this.CurrentTrack = track;
+//     this.IsPaused = false;
+//    };
+//     this.Pause = function () {
+//        this.IsPaused = !this.IsPaused;
+//    };
+//   }]);
+//
+// })
