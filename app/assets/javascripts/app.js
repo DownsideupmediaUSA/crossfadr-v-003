b6ba7f06c771af4a933d6e6832c3ba25058e641c
DownsideupmediaUSA <brandondecarlo@live.com>
2017-04-26 09:36:34 -0400

    abstracted routes from app file into routes.js file

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,50 +1,59 @@
+(function() {
+  'use strict';
+
+  angular
+         .module('crossfadr', ['ui.router', 'templates']);
+}());
+
+
+
 var app = angular.module('crossfadr', ['ui.router', 'templates']);
 
-app
-   .config(['$stateProvider', '$urlRouterProvider',
-   function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('home', {
-          url:'/home',
-          templateUrl: 'main/home.html',
-          controller: 'MainCtrl as ctrl'
-        })
-        .state('home.tracks', {
-          url:'/tracks',
-          templateUrl: 'mixes/mixes.html',
-          controller: 'MixCntrl as ctrl'
-        })
-        .state('home.artists', {
-          url:'/artists',
-          templateUrl: 'artists/artist.html',
-          controller: 'ArtistCntrl as ctrl'
-        })
-        // .state('home.track', {
-        //   url:'/:id',
-        //   templateUrl: 'mixes/show.html',
-        //   controller: 'mixDetailCntrl as ctrl'
-        // })
-        .state('home.new', {
-          url: '/new',
-          templateUrl: 'mixes/new.html',
-
-        })
-        .state('home.about', {
-          url:'/about',
-          templateUrl: 'static/about.html',
-          controller: 'AboutCntrl as ctrl'
-        })
-        .state('home.contact', {
-          url:'/contact',
-          templateUrl: 'main/contact.html',
-          controller: 'contactCtrl as ctrl'
-        });
-        // .state('home.jplayer', {
-        //   url:'/jplayer',
-        //   templateUrl: 'mixes/jplayer.html',
-        //   controller: 'jPlayerCtrl as ctrl'
-        // });
-
-
-    $urlRouterProvider.otherwise('/home');
-  }])
+// app
+//    .config(['$stateProvider', '$urlRouterProvider',
+//    function($stateProvider, $urlRouterProvider) {
+//     $stateProvider
+//         .state('home', {
+//           url:'/home',
+//           templateUrl: 'main/home.html',
+//           controller: 'MainCtrl as ctrl'
+//         })
+//         .state('home.tracks', {
+//           url:'/tracks',
+//           templateUrl: 'mixes/mixes.html',
+//           controller: 'MixCntrl as ctrl'
+//         })
+//         .state('home.artists', {
+//           url:'/artists',
+//           templateUrl: 'artists/artist.html',
+//           controller: 'ArtistCntrl as ctrl'
+//         })
+//         // .state('home.track', {
+//         //   url:'/:id',
+//         //   templateUrl: 'mixes/show.html',
+//         //   controller: 'mixDetailCntrl as ctrl'
+//         // })
+//         .state('home.new', {
+//           url: '/new',
+//           templateUrl: 'mixes/new.html',
+//
+//         })
+//         .state('home.about', {
+//           url:'/about',
+//           templateUrl: 'static/about.html',
+//           controller: 'AboutCntrl as ctrl'
+//         })
+//         .state('home.contact', {
+//           url:'/contact',
+//           templateUrl: 'main/contact.html',
+//           controller: 'contactCtrl as ctrl'
+//         });
+//         // .state('home.jplayer', {
+//         //   url:'/jplayer',
+//         //   templateUrl: 'mixes/jplayer.html',
+//         //   controller: 'jPlayerCtrl as ctrl'
+//         // });
+//
+//
+//     $urlRouterProvider.otherwise('/home');
+//   }])
--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,35 +1,54 @@
-// (function(){
-//   'use strict';
-//
-//   angular
-//       .module('crossfadr')
-//       .config(function($stateProvider, $urlRouterProvider) {
-//        $stateProvider
-//        .state('home', {
-//          url:'/home',
-//          templateUrl: 'main/home.html',
-//          controller: 'MainCtrl as vm'
-//        })
-//        .state('tracks', {
-//          url:'/tracks',
-//          templateUrl: 'mixes/mixes.html',
-//          controller: 'MixCntrl as vm'
-//        })
-//        .state('artists', {
-//          url:'/artists',
-//          templateUrl: 'djs/djs.html',
-//          controller: 'DJCntrl as vm'
-//        })
-//        .state('about', {
-//          url:'about',
-//          templateUrl: 'home/about.html',
-//        })
-//        .state('contact', {
-//          url:'contact',
-//          templateUrl: 'home/contact.html',
-//        });
-//
-//        $urlRouterProvider.otherwise('/home')
-//     })
-//
-// }());
+(function(){
+  'use strict';
+
+  angular
+      .module('crossfadr')
+      .config(['$stateProvider', '$urlRouterProvider',
+      function($stateProvider, $urlRouterProvider) {
+       $stateProvider
+           .state('home', {
+             url:'/home',
+             templateUrl: 'main/home.html',
+             controller: 'MainCtrl as ctrl'
+           })
+           .state('home.tracks', {
+             url:'/tracks',
+             templateUrl: 'mixes/mixes.html',
+             controller: 'MixCntrl as ctrl'
+           })
+           .state('home.artists', {
+             url:'/artists',
+             templateUrl: 'artists/artist.html',
+             controller: 'ArtistCntrl as ctrl'
+           })
+           // .state('home.track', {
+           //   url:'/:id',
+           //   templateUrl: 'mixes/show.html',
+           //   controller: 'mixDetailCntrl as ctrl'
+           // })
+           .state('home.new', {
+             url: '/new',
+             templateUrl: 'mixes/new.html',
+
+           })
+           .state('home.about', {
+             url:'/about',
+             templateUrl: 'static/about.html',
+             controller: 'AboutCntrl as ctrl'
+           })
+           .state('home.contact', {
+             url:'/contact',
+             templateUrl: 'main/contact.html',
+             controller: 'contactCtrl as ctrl'
+           });
+           // .state('home.jplayer', {
+           //   url:'/jplayer',
+           //   templateUrl: 'mixes/jplayer.html',
+           //   controller: 'jPlayerCtrl as ctrl'
+           // });
+
+
+       $urlRouterProvider.otherwise('/home');
+     }])
+
+}());
