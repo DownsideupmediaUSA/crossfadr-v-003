3e3dd33cefb442ef425db11a3101e2ae1cf9ee7b
DownsideupmediaUSA <brandondecarlo@live.com>
2017-05-06 09:26:42 -0400

    more work on devise angular

--- app/assets/javascripts/main/MainCtrl.js
+++ app/assets/javascripts/main/MainCtrl.js
@@ -1,27 +1,29 @@
 (function() {
 
-  'use strict';
+  'use strict'
   angular
          .module('crossfadr')
          .controller('MainCtrl', MainCtrl)
 
           function MainCtrl(Auth) {
               var vm = this
+              vm.logout = Auth.logout
 
               var credentials = {
                 username: 'bdc',
                 email: 'bdc@bdc.com',
                 password: 'password'
-              };
+              }
               var config = {
                 headers: {
                   'X-HTTP-Method-Override':
                   'POST'
                 }
-              };
+              }
 
                     Auth.login(credentials, config)
                         .then(function(user) {
+                            vm.user = user
                             console.log(user)
                           }, function(error) {
                             console.log(error)
@@ -29,10 +31,19 @@
 
                     Auth.currentUser()
                         .then(function(user) {
+                          vm.user = user
                           console.log(user)
                         }, function(error) {
                           console.log(error)
 
                         })
-          }
+                    }
+                        // $scope.$on('devise:logout', function(event, oldCurrentUser) {
+                        //   vm.user = {}
+                        // })
+
+
+
+
+        
 }())
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
+  protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  after_filter :set_csrf_cookie_for_ng
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
   def home
     render 'layouts/application'
   end
 
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation,
+    :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def verified_request?
+    super || valid_authenticity_token?(session,
+    request.headers['X-XSRF-TOKEN'])
+  end
 end
