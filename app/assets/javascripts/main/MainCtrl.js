c3963f713291c71ab9427f4a63d7c8cfc77e984c
DownsideupmediaUSA <brandondecarlo@live.com>
2017-05-07 08:15:55 -0400

    set up logout functionality

--- app/assets/javascripts/main/MainCtrl.js
+++ app/assets/javascripts/main/MainCtrl.js
@@ -1,11 +1,12 @@
 (function() {
 
-  'use strict'
+  'use strict';
+
   angular
          .module('crossfadr')
          .controller('MainCtrl', MainCtrl)
 
-          function MainCtrl(Auth) {
+          function MainCtrl(Auth, $scope) {
               var vm = this
               vm.logout = Auth.logout
 
@@ -14,12 +15,14 @@
                 email: 'bdc@bdc.com',
                 password: 'password'
               }
+
               var config = {
-                headers: {
-                  'X-HTTP-Method-Override':
-                  'POST'
-                }
+                  headers: {
+                    'X-HTTP-Method-Override': 'POST'
+                  }
               }
+
+
 
                     Auth.login(credentials, config)
                         .then(function(user) {
@@ -37,13 +40,26 @@
                           console.log(error)
 
                         })
+
+
+                    Auth.logout(config).then(function(oldUser) {
+                      // alert(oldUser.name + "you're signed out now.");
+                     }, function(error) {
+                      // An error occurred logging out.
+                     });
+
+
+
+                    $scope.$on('devise:logout', function(event, oldCurrentUser) {
+                      vm.user = {}
+
+                    });
+
                     }
-                        // $scope.$on('devise:logout', function(event, oldCurrentUser) {
-                        //   vm.user = {}
-                        // })
 
 
 
 
-        
-}())
+
+
+}());
